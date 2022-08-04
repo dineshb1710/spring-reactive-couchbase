e6a7f4af7001025e482580483e75bd82dc5fa348
dineshb1710 <dinesh.bhardwaj1710@gmail.com>
2022-08-04 16:40:31 +0200

    Added Initial Project

---
+++ src/main/java/com/practice/cb/SpringReactiveCouchbaseApplication.java
@@ -0,0 +1,13 @@
+package com.practice.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveCouchbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveCouchbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/cb/SpringReactiveCouchbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveCouchbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
